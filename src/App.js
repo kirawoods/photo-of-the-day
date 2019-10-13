96184abf06ab3e9fd8ddc3cb4cc3db2daaf8eb5e
Kira <kawoods826@gmail.com>
2019-10-12 18:34:48 -0700

    added photo component file and installed axios library

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios'; 
+
+axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
 
 function App() {
   return (
@@ -8,5 +22,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
